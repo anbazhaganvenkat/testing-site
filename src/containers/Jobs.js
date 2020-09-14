e4dd2ac04cc8ed774bdf9d83bf67de53c34b205b
AngularTeamSzigony <angularteam@szigony.in>
2020-09-15 02:02:47 +0530

    initial

--- src/containers/Jobs.js
+++ src/containers/Jobs.js
@@ -11,9 +11,6 @@ class Jobs extends Component {
               <div className="container">
                 <div className="row">
                   <div className="col-md-12 text-center mt-sm-2 mt-lg-0 mt-xs-0 mt-3 mt-md-0">
-                    <h4>
-                      <span className="text-danger">ThiDiff ?</span>
-                    </h4>
                     <p>
                       We do not offer job. We give platform for you to build
                       your career.
